ff7e2797c8e6d818693382fd20a4d155650ae9d9
Iurii Rychkov <irychkov@student.hive.fi>
2024-05-05 14:10:04 +0300

    Add count as returning value. Implement str. Test.

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,53 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 21:54:14 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/30 09:30:32 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/05/05 14:08:08 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdio.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c, int *counter)
 {
 	write(1, &c, 1);
+	*counter = *counter + 1;
+}
+
+void	ft_putstr(char *s, int *counter)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+		*counter = *counter + 1;
+	}
 }
 
 int	ft_printf(const char *format, ...)
 {
 	size_t	i;
+	int	counter;
 	va_list	args;
-	va_start(args, format);
 
+	va_start(args, format);
 	//validation for format?
 	i = 0;
+	counter = 0;
 	while (format[i])
 	{
-		if (format[i] == '%') 
+		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == 'c')
-				ft_putchar(va_arg(args, char));
+				ft_putchar(va_arg(args, int), &counter);
 			else if (format[i] == 's')
-				continue; //temp print string
+				ft_putstr(va_arg(args, char *), &counter);
 			else if (format[i] == 'p')
 				continue; //temp print pointer
 			else if (format[i] == 'd')
@@ -50,18 +69,27 @@ int	ft_printf(const char *format, ...)
 				continue; //temp print %
 		}
 		else
-			ft_putchar(format[i]);
+		{
+			ft_putchar(format[i], &counter);
+		}
 		i++;
 	}
 	va_end(args);
-	return 0; //temp not true
+	printf("mine return - %d\n", counter);
+	return (0); //temp not true
 
 }
 
-/* int	main(void)
+int	main(void)
 {
 	char	c;
+	char	s[20] = "! Welcome";
 
 	c = '!';
-	ft_printf("Hello Iurii%c", c);
-} */
+	//Test for char
+	ft_printf("Hello Iurii%c\n", c);
+	printf("original return - %d\n", printf("Hello Iurii%c\n", c));
+	//Test for string
+	ft_printf("\nHello Iurii%s\n", s);
+	printf("original return - %d\n", printf("\nHello Iurii%s\n", s));
+}
