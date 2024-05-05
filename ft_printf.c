b93676d0d3784ba2abaef6098b84f0ba83d9703f
Iurii Rychkov <irychkov@student.hive.fi>
2024-05-05 18:25:45 +0300

    Add puthex, putptr.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 21:54:14 by irychkov          #+#    #+#             */
-/*   Updated: 2024/05/05 15:10:05 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/05/05 18:24:05 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,8 @@ void	ft_putstr(char *s, int *counter)
 		return ;
 	while (s[i])
 	{
-		write(1, &s[i], 1);
+		ft_putchar(s[i], counter);
 		i++;
-		*counter = *counter + 1;
 	}
 }
 
@@ -46,15 +45,13 @@ void	ft_putnbr(int n, int *counter)
 	}
 	else if (n < 0)
 	{
-		write(1, "-", 1);
-		*counter = *counter + 1;
+		ft_putchar('-', counter);
 		n = -n;
 	}
 	if (n >= 10)
 		ft_putnbr(n / 10, counter);
 	c = n % 10 + '0';
-	*counter = *counter + 1;
-	write(1, &c, 1);
+	ft_putchar(c, counter);
 }
 
 void	ft_putunsnbr(unsigned int n, int *counter)
@@ -64,8 +61,51 @@ void	ft_putunsnbr(unsigned int n, int *counter)
 	if (n >= 10)
 		ft_putunsnbr(n / 10, counter);
 	c = n % 10 + '0';
-	*counter = *counter + 1;
-	write(1, &c, 1);
+	ft_putchar(c, counter);
+}
+
+void	ft_puthex(unsigned int n, int *counter, char format)
+{
+	char	c;
+
+	if (n >= 16)
+	{
+		ft_puthex(n / 16, counter, format);
+		ft_puthex(n % 16, counter, format);
+	}
+	else
+	{
+		if (n < 10)
+			c = n + '0';
+		else if (format == 'X')
+			c = n - 10 + 'A';
+		else if (format == 'x')
+			c = n - 10 + 'a';
+		write(1, &c, 1);
+		*counter = *counter + 1;
+	}
+}
+
+void	ft_putptr(void *p, int *counter)
+{
+	char			c;
+	unsigned long	ptr;
+
+	ptr = (unsigned long)p;
+	if (ptr >= 16)
+	{
+		ft_putptr((void *)(ptr / 16), counter);
+		ft_putptr((void *)(ptr % 16), counter);
+	}
+	else 
+	{
+		if (ptr < 10)
+			c = ptr + '0';
+		else
+			c = ptr - 10 + 'a';
+		write(1, &c, 1);
+		*counter = *counter + 1;
+	}
 }
 
 int	ft_printf(const char *format, ...)
@@ -88,7 +128,11 @@ int	ft_printf(const char *format, ...)
 			else if (format[i] == 's')
 				ft_putstr(va_arg(args, char *), &counter);
 			else if (format[i] == 'p')
-				continue; //temp print pointer
+			{
+				write(1, "0x", 2);
+				counter += 2;
+				ft_putptr(va_arg(args, void *), &counter);
+			}
 			else if (format[i] == 'd')
 				ft_putnbr(va_arg(args, int), &counter);
 			else if (format[i] == 'i')
@@ -96,11 +140,11 @@ int	ft_printf(const char *format, ...)
 			else if (format[i] == 'u')
 				ft_putunsnbr(va_arg(args, unsigned int), &counter);
 			else if (format[i] == 'x')
-				continue; //temp print hex lowercase
+				ft_puthex(va_arg(args, unsigned int), &counter, format[i]);
 			else if (format[i] == 'X')
-				continue; //temp print hex uppercase
+				ft_puthex(va_arg(args, unsigned int), &counter, format[i]);
 			else if (format[i] == '%')
-				continue; //temp print %
+				ft_putchar('%', &counter);
 		}
 		else
 		{
@@ -117,6 +161,7 @@ int	ft_printf(const char *format, ...)
 int	main(void)
 {
 	char			c;
+	char 			*ptr;
 	int				number;
 	int				number2;
 	unsigned int	number3;
@@ -126,6 +171,7 @@ int	main(void)
 	number = -2147483648;
 	number2 = 2147483647;
 	number3 = 4294967295;
+	ptr = s;
 
 	//Test for %c
 	ft_printf("Hello Iurii%c\n", c);
@@ -146,4 +192,16 @@ int	main(void)
 	//Test for %u
 	ft_printf("\n%u\n", number3);
 	printf("original return - %d\n", printf("\n%u\n", number3));
+
+	//Test for %x
+	ft_printf("\n%x\n", number3);
+	printf("original return - %d\n", printf("\n%x\n", number3));
+
+	//Test for %X
+	ft_printf("\n%X\n", number3);
+	printf("original return - %d\n", printf("\n%X\n", number3));
+
+	//Test for %p
+	ft_printf("\n%p\n", ptr);
+	printf("original return - %d\n", printf("\n%p\n", ptr));
 }
