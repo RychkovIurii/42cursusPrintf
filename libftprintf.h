875fe8506100dc1212e83b34f90558346aa27b4c
Iurii Rychkov <irychkov@student.hive.fi>
2024-05-06 15:58:04 +0300

    Add 6 functions prototypes.

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 21:45:55 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/29 22:48:24 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/05/06 15:53:53 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
+void	ft_putchar(char c, int *counter);
+void	ft_putstr(char *s, int *counter);
+void	ft_putnbr(int n, int *counter);
+void	ft_putunsnbr(unsigned int n, int *counter);
+void	ft_puthex(unsigned int n, int *counter, char format);
+void	ft_putptr(void *p, int *counter);
+int		ft_printf(const char *format, ...);
 
 #endif
