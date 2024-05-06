bd8e01f033c35fe3bd1ce9a7df7426cb649f1ccd
Iurii Rychkov <irychkov@student.hive.fi>
2024-05-06 15:58:59 +0300

    Add validation for %%. Separate files

--- ft_printf.c
+++ ft_printf.c
@@ -6,112 +6,17 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 21:54:14 by irychkov          #+#    #+#             */
-/*   Updated: 2024/05/05 18:24:05 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/05/06 15:57:02 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
-void	ft_putchar(char c, int *counter)
-{
-	write(1, &c, 1);
-	*counter = *counter + 1;
-}
-
-void	ft_putstr(char *s, int *counter)
-{
-	size_t	i;
-
-	i = 0;
-	if (!s)
-		return ;
-	while (s[i])
-	{
-		ft_putchar(s[i], counter);
-		i++;
-	}
-}
-
-void	ft_putnbr(int n, int *counter)
-{
-	char	c;
-
-	if (n == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		*counter = *counter + 11;
-		return ;
-	}
-	else if (n < 0)
-	{
-		ft_putchar('-', counter);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10, counter);
-	c = n % 10 + '0';
-	ft_putchar(c, counter);
-}
-
-void	ft_putunsnbr(unsigned int n, int *counter)
-{
-	char	c;
-
-	if (n >= 10)
-		ft_putunsnbr(n / 10, counter);
-	c = n % 10 + '0';
-	ft_putchar(c, counter);
-}
-
-void	ft_puthex(unsigned int n, int *counter, char format)
-{
-	char	c;
-
-	if (n >= 16)
-	{
-		ft_puthex(n / 16, counter, format);
-		ft_puthex(n % 16, counter, format);
-	}
-	else
-	{
-		if (n < 10)
-			c = n + '0';
-		else if (format == 'X')
-			c = n - 10 + 'A';
-		else if (format == 'x')
-			c = n - 10 + 'a';
-		write(1, &c, 1);
-		*counter = *counter + 1;
-	}
-}
-
-void	ft_putptr(void *p, int *counter)
-{
-	char			c;
-	unsigned long	ptr;
-
-	ptr = (unsigned long)p;
-	if (ptr >= 16)
-	{
-		ft_putptr((void *)(ptr / 16), counter);
-		ft_putptr((void *)(ptr % 16), counter);
-	}
-	else 
-	{
-		if (ptr < 10)
-			c = ptr + '0';
-		else
-			c = ptr - 10 + 'a';
-		write(1, &c, 1);
-		*counter = *counter + 1;
-	}
-}
-
 int	ft_printf(const char *format, ...)
 {
 	size_t	i;
-	int	counter;
+	int		counter;
 	va_list	args;
 
 	va_start(args, format);
@@ -120,7 +25,7 @@ int	ft_printf(const char *format, ...)
 	counter = 0;
 	while (format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			if (format[i] == 'c')
@@ -153,9 +58,7 @@ int	ft_printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	printf("mine return - %d\n", counter);
-	return (0); //temp not true
-
+	return (counter);
 }
 
 int	main(void)
@@ -174,34 +77,47 @@ int	main(void)
 	ptr = s;
 
 	//Test for %c
-	ft_printf("Hello Iurii%c\n", c);
+	ft_printf("mine return - %d\n", ft_printf("Hello Iurii%c\n", c));
 	printf("original return - %d\n", printf("Hello Iurii%c\n", c));
 
 	//Test for %s
-	ft_printf("\nHello Iurii%s\n", s);
+	ft_printf("mine return - %d\n", ft_printf("\nHello Iurii%s\n", s));
 	printf("original return - %d\n", printf("\nHello Iurii%s\n", s));
 
 	//Test for %d
-	ft_printf("\n%d\n", number);
+	ft_printf("mine return - %d\n", ft_printf("\n%d\n", number));
 	printf("original return - %d\n", printf("\n%d\n", number));
 
 	//Test for %i
-	ft_printf("\n%i\n", number2);
+	ft_printf("mine return - %d\n", ft_printf("\n%i\n", number2));
 	printf("original return - %d\n", printf("\n%i\n", number2));
 
 	//Test for %u
-	ft_printf("\n%u\n", number3);
+	ft_printf("mine return - %d\n", ft_printf("\n%u\n", number3));
 	printf("original return - %d\n", printf("\n%u\n", number3));
 
 	//Test for %x
-	ft_printf("\n%x\n", number3);
+	ft_printf("mine return - %d\n", ft_printf("\n%x\n", number3));
 	printf("original return - %d\n", printf("\n%x\n", number3));
 
 	//Test for %X
-	ft_printf("\n%X\n", number3);
+	ft_printf("mine return - %d\n", ft_printf("\n%X\n", number3));
 	printf("original return - %d\n", printf("\n%X\n", number3));
 
 	//Test for %p
-	ft_printf("\n%p\n", ptr);
+	ft_printf("mine return - %d\n", ft_printf("\n%p\n", ptr));
 	printf("original return - %d\n", printf("\n%p\n", ptr));
+
+	//Test for %%
+	ft_printf("mine return - %d\n", ft_printf("%%"));
+	printf("original return - %d\n", printf("%%"));
+
+	//Test for ""
+	ft_printf("mine return - %d\n", ft_printf(""));
+	printf("original return - %d\n", printf(""));
+
+	//Test for error
+	ft_printf("mine return - %d\n", ft_printf("%d"));
+	//printf("original return - %d\n", printf("%d"));			CHECK ERROR. Original function has diff behavior.
+
 }
